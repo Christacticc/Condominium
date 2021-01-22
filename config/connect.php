a2d360ca984bfebe78dafa9364a4991cd552e67c
Christacticc <christophe@tacticc.com>
2021-01-22 15:28:14 +0100

    gitignore

--- config/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbhost = 'tacticcc0nd0.mysql.db';
-$dbname = 'tacticcc0nd0';
-$dbuser = 'tacticcc0nd0';
-$dbpw = '98dQZiG3apn4Ezj';
-$db = new PDO('mysql:host=' . $dbhost .';dbname=' . $dbname .';charset=utf8', $dbuser, $dbpw);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
--- config/vars.php
+++
@@ -1,2 +0,0 @@
-<?php
-$recaptchakey = '6LdC--4UAAAAACU9a4nqU-hmxWcIOS9SoWkxNj6x';
